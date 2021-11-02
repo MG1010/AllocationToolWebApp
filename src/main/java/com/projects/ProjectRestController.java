a8909195efa401f14ba7a407b3f73e6514544f91
Marek Grzesiak <Marek.Grzesiak@gds.ey.com>
2021-11-02 19:15:16 +0100

    REST Methods: Delete & Put added

--- src/main/java/com/projects/ProjectRestController.java
+++ src/main/java/com/projects/ProjectRestController.java
@@ -14,14 +14,36 @@ public class ProjectRestController {
         this.repository = repository;
     }
 
-    @GetMapping(value = "api/projects")
+    @GetMapping(value = "/api/projects")
     public List<Project> retrieveAllProjects() {
         return repository.findAll();
     }
 
-    @PostMapping(value = "api/addproject")
-    public void addProject(@RequestBody Project project) {
-        repository.save(project);
+    @GetMapping(value = "/api/projects/{id}")
+    public Project retrieveProjectById(@PathVariable int id) {
+        return repository.findById(id);
     }
+
+    @PostMapping(value = "/api/addproject")
+    public Project addProject(@RequestBody Project newProject) {
+        return repository.save(newProject);
+    }
+
+    @PutMapping("/api/projects/{id}")
+    public Project updateProject(@RequestBody Project newProject, @PathVariable int id) {
+
+        Project project = repository.findById(id);
+
+        project.setName(newProject.getName());
+        project.setDescription(newProject.getDescription());
+
+        return repository.save(project);
+    }
+
+    @DeleteMapping("/api/projects/{id}")
+    void deleteProject(@PathVariable int id) {
+        repository.deleteById(id);
+    }
+
 
 }
